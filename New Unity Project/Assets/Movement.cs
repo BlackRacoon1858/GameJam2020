eb8414d63ed1f3b867463b4b3252f0e8b8bb3d40
LAPTOP-5835AAD2\ferna <fernandojmichel@outlook.com>
2020-10-17 11:03:15 -0600

    Base

---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+
+    public float speed = 25.0f;
+    public float jump = 5.0f;
+    public float run = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        if(hor != 0.0f || ver != 0.0f)
+        {
+            Vector3 dir = transform.forward * ver + transform.right * hor;
+
+            if (Input.GetKey("left shift"))
+            {
+                rigidbody.MovePosition(transform.position + dir * run * Time.deltaTime);
+            }
+            else 
+            {
+                rigidbody.MovePosition(transform.position + dir * speed * Time.deltaTime);
+            }
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigidbody.velocity = new Vector3(0, jump, 0);
+        }
+    }
+}
